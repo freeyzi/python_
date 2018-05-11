a2ae2f8b1b89c185c8b39ed18855e90dc41daae7
freeyzi <wikop90@gmail.com>
2018-05-11 18:17:22 +0800

    。

--- python爬虫/defxinlan.py
+++ python爬虫/defxinlan.py
@@ -7,13 +7,15 @@ def xinlang():
 		for html in htmls.items():
 			href = html.attr('href')
 			text = html.text()
-			heers = text + '\n' + href
+			heers = text + '\n' + href + "\n"
 			print("抓爬中:===>>>>>>",heers)
 
 			with open("新浪.txt",'a',encoding="utf-8") as f:
 				f.write(heers)
 				f.close()
 				print("爬取成功")
+		else:
+			print("运行完成！")
 	except:
 		print("====.====")
 
